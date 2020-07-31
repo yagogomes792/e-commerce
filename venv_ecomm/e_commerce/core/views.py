ac58a0bef9db8ed250fc7d9ebf762b6321e9098e
yagogomes792 <yago.792@hotmail.com>
2020-07-31 10:31:20 -0300

    criado classe para exibir detalhes do produto e adicionado secret_key

--- venv_ecomm/e_commerce/core/views.py
+++ venv_ecomm/e_commerce/core/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from .forms import ContactForm, LoginForm, RegisterForm
 from django.contrib.auth import login, get_user_model, authenticate
 from core.models import Product
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
 def home_page(request):
@@ -73,4 +73,12 @@ class ProductListView(ListView):
 
     def get_context_data(self, *args, **kwargs):
         context = super(ProductListView, self).get_context_data(*args, **kwargs)
+        return context
+
+class ProductDetailView(DetailView):
+    queryset = Product.objects.all()
+    template_name = 'products/detail.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
         return context
