decc024c859df817c277d4af8a9bacf01bb1ce87
yagogomes792 <yago.792@hotmail.com>
2020-08-04 16:45:57 -0300

    adicionado menu navbar

--- venv_ecomm/e_commerce/core/urls.py
+++ venv_ecomm/e_commerce/core/urls.py
@@ -4,6 +4,6 @@ from core.views import ProductDetailSlugView, ProductListView
 app_name = "products"
 
 urlpatterns = [
-    path('', ProductListView.as_view()),
+    path('', ProductListView.as_view(), name='list'),
     path('<slug:slug>/', ProductDetailSlugView.as_view(), name='detail')
 ]
